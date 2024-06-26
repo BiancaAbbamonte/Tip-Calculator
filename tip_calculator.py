dea5fa83268dfaf1db0472c85bdcb96e8fb09a56
Bianca Abbamonte Aveno <bianca.abbamonte@yahoo.com.br>
2024-06-25 23:14:39 -0300

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,11 +2,11 @@ print("Welcome to the Tip Calculator!\n")
 
 bill = float(input("What was the total bill?\n$"))
 tip = int(input("How much tip would you like to give? 10%, 12%, 15%?\n"))
-numberPeople = int(input("How many people to split the bill?\n"))
+number_people = int(input("How many people to split the bill?\n"))
 
-finalValue = round(((bill / numberPeople) * (1 + tip/100)),2)
+fina_value = round(((bill / numberPeople) * (1 + tip/100)),2)
 
-#esse step é apenas para garantir as duas após a virgula
-final_amount = "{:.2f}".format(finalValue)
+#This step is just to make sure there is to values after the comma
+final_amount = "{:.2f}".format(final_value)
 
 print(f"Cada pessoa vai pagar: ${final_amount}")
